31805731836c1604bf9449016eb6edf6aebd9ad7
Luke Dallenbach <lukedallenbach87@gmail.com>
2023-07-11 00:50:53 -0600

    changed the ordering of some loops, still needs a lot of work

--- password generator.py
+++ password generator.py
@@ -10,7 +10,23 @@ def password_generator():
     def password_necessities():
         print('Passwords must contain at least one of each of these elements:')
         print('Passwords must include at least 1 of the following: \nA capital letter\nOne number \nAt least one special character')
-        print('Passwords must have a minimum length of 8 characters.\n')
+        print('Passwords must have a minimum length of 8 characters.')
+
+        def generate_password(length = 12):
+            characters = string.ascii_letters + string.digits + punctuation
+            password = ''.join(random.choice(characters) for _ in range(length))
+
+            print('\nIf you need help creating a password, one can be created for you.')
+            create_password = input('If you would like a password created for you, please press 1: \n')
+    
+            if create_password == '1':
+                generated_password = password
+                print('Generated Password:', generated_password)
+                print('You can copy and paste this password.')
+        
+            else:
+                print('Sounds good. Please continue.')
+        generate_password(length = 12)
         
         user_password = input('Please enter your password below: \n')
 
@@ -24,47 +40,57 @@ def password_generator():
         else:
             print('This password does not meet the necessary requirements, please try again.')
 
-    password_necessities()    
-
-
-    #code below needs adjusting 
-    print('If you need help creating a password, one can be created for you.')
-    create_password = input('If you would like a password created for you, please press 1: \n')
-    
-    #if create_password == '1':
-    #    generated_password = generate_password()
-     #   print('Generated Password:', generated_password)
-    
-
-    def generate_password(length=8):
-        characters = string.ascii_letters + string.digits + punctuation
-        password = ''.join(random.choice(characters) for _ in range(length))
-        print(password)
-    generate_password(length=8)
-
+    password_necessities() 
 
     def saved_passwords_and_user_names():
-        ssaved_user_passwords = []
+        saved_user_passwords = []
         saved_user_names = {}
 
         websites = True
         while websites:
             new_keys = input('Please enter the website below: \n')
-            need_more = input('Are there any more websites you want the user names remembered for? Select y or n below: \n')
-            if need_more == "n":
-                websites = False
-            
+            need_more = input('Are there any more websites you want the usernames remembered for? Select yes or no below: \n')
+            if need_more != 'no' and need_more != 'yes':
+                print('Must choose either yes or no.')
+            else:
+                if need_more == 'no':
+                    websites = False
 
         user_names = True
         while user_names:
             new_values = input('Please enter the usernames for the websites in the same corresponding order below: \n')
-            need_more_user_names = input('Any more user names needed to be saved? Select y or n below: \n')
-            if need_more_user_names == 'n':
-                user_names = False
+            need_more_user_names = input('Any more user names needed to be saved? Select yes or no below: \n')
 
-        saved_user_names[new_keys] = new_values 
-        print(saved_user_names)
+            if need_more_user_names != 'no' and need_more_user_names != 'yes':
+                print('Must choose either yes or no.')
+            else:
+                if need_more_user_names == 'no':
+                    user_names = False
+
+            saved_user_names[new_keys] = new_values 
+            print(saved_user_names)
+
+        passwords = True
+        while passwords:
+            saved_passwords = input('Do you want to save this password? Yes or no \n')
+            if saved_passwords != 'yes' and saved_passwords != 'no':
+                print('You must select a valid choice.')
+            else:
+                if saved_passwords == 'no':
+                    print('Okay sounds good yo')
+                    passwords = False
+                else:
+                    while True:
+                        the_passwords = input("Please enter the passwords below. Once finished, please type 'stop' \n")
+                        print('Password saved')
+                        if the_passwords == 'stop':
+                            passwords = False
+                            break
+
+            print(the_passwords)
+
 
     saved_passwords_and_user_names()
 
 password_generator()
+
